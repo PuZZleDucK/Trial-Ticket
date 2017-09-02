441cdcba132011a842ef67c6913f8cdf46ee76e1
PuZZleDucK <puzzleduck@gmail.com>
2017-09-03 09:52:57 +1000

    deterministic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-products = Product.create([
+Product.destroy_all
+Product.create([
   { title: "item one", price: 111},
   { title: "item 2", price: 122},
   { title: "item 3", price: 133},
   { title: "item fore", price: 144},
   ])
 
-store_products = StoreProduct.create([
+StoreProduct.destroy_all
+StoreProduct.create([
     { title: "store one", price: 111},
     { title: "store 2", price: 122}
     ])
