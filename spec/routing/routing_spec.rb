f960b4e83d9ae2010b965bfc257a3fd6eae02049
PuZZleDucK <puzzleduck@gmail.com>
2017-09-03 12:53:36 +1000

    implement routing tests

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'net/http'
+require 'uri'
+require 'json'
 
+describe "Routing", type: :request do
 
-RSpec.describe "Routing", type: :request do
+  it "Fetching without params gets error" do
+    get "/graphql"
+    expect(response.body).to eq '{"errors":[{"message":"No query string was present"}]}'
+  end
 
   it "Fetching main endpoint responds" do
-     # fetch main endpoint
-     # verify it's not an error
-     expect(true).to eq true #todo
+    # fetch main endpoint
+    post "/graphql", :query => ""
+    # verify response
+    expect(response.body).to eq "{}"
+    # verify it's not an error
+    expect(response.status).to eq 200
   end
 
 end
