ca59c646c0230b0e17ed50f9ed3625a1fd61a7ba
PuZZleDucK <puzzleduck@gmail.com>
2017-09-03 14:11:14 +1000

    add expiry date test

--- spec/models/store_product_spec.rb
+++ spec/models/store_product_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe StoreProduct, type: :model do
 
       it "Store Products can be created" do
-         product = StoreProduct.new({ title: "item one", price: 111});
+         product = StoreProduct.new({ title: "item one", price: 111, expiring: Time.new(2017, 12, 31, 12, 0, 0)});
          expect(product.title).to eq "item one"
          expect(product.price).to eq 111
+         expect(product.expiring).to eq Time.new(2017, 12, 31, 12, 0, 0)
       end
 
 end
